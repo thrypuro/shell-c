c76dd8f7c0172c638bab7e7b2b9acc5d04387e33
thrypuro <56890082+thrypuro@users.noreply.github.com>
2026-01-18 14:42:18 +0000

    Exit implemented

--- src/main.c
+++ src/main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX_SHELL_INPUT 128
+#define EXIT_STRING "exit"
+
+typedef enum commands {
+  EXIT,
+  INVALID
+} commands;
+
+commands parse(char pInp[MAX_SHELL_INPUT]){
+  int aBool = strcmp(pInp,EXIT_STRING);
+  if (aBool == 0){
+    return EXIT;
+  }
+  return INVALID;
+}
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
@@ -12,6 +27,10 @@ int main(int argc, char *argv[]) {
     // TODO: Uncomment the code below to pass the first stage
     printf("$ ");
     scanf("%127s",aBuffer);
+    commands aParsed = parse(aBuffer);
+    if (aParsed == EXIT) {
+      break;
+    }
     printf("%s: command not found\n",aBuffer);
   }
   return 0;
