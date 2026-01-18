90e59097f82ff37af8b2e08339217100b78b2fc2
thrypuro <56890082+thrypuro@users.noreply.github.com>
2026-01-18 14:29:59 +0000

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -6,11 +6,13 @@
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
-  char aBuffer[MAX_SHELL_INPUT];
-  // TODO: Uncomment the code below to pass the first stage
-  printf("$ ");
-  scanf("%127s",aBuffer);
-  printf("%s: command not found\n",aBuffer);
 
+  while (1){
+    char aBuffer[MAX_SHELL_INPUT];
+    // TODO: Uncomment the code below to pass the first stage
+    printf("$ ");
+    scanf("%127s",aBuffer);
+    printf("%s: command not found\n",aBuffer);
+  }
   return 0;
 }
