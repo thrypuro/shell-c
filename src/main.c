31b23eec88210c0c0a904b63874afccdf3787c78
thrypuro <56890082+thrypuro@users.noreply.github.com>
2026-01-18 14:27:28 +0000

    Stage 2

--- src/main.c
+++ src/main.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_SHELL_INPUT 128
+
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
-
+  char aBuffer[MAX_SHELL_INPUT];
   // TODO: Uncomment the code below to pass the first stage
   printf("$ ");
+  scanf("%127s",aBuffer);
+  printf("%s: command not found\n",aBuffer);
 
   return 0;
 }
